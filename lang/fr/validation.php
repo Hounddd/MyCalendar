e28743ed7cc7e324fccbf8bddaddead0a85969ca
Hounddd <contact@hounddd.fr>
2018-01-18 19:49:10 +0100

    French translation added
    French translation added

---
+++ lang/fr/validation.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'INTERVAL.required' => 'Un intervalle est nécessaire pour la répétition d\'événement sélectionnée.',
+    'INTERVAL.integer' => 'L\'intervalle de répétition doit être un entier valide.',
+    'INTERVAL.min' => 'L\'intervalle de répétition doit être d\'au moins 1.',
+
+    'WBYDAY.required' => 'Au moins un jour de la semaine doit être sélectionné pour les événements répétitifs hebdomadaires.',
+
+    'BYDAY.required' => 'Au moins un jour de semaine doit être sélectionné pour la répétition d\'événement sélectionnée.',
+
+    'YBYMONTH.required' => 'Vous devez spécifier un mois valide pour les événements répétitifs annuels.',
+    'YBYDAY.required' => 'Au moins un jour de la semaine doit être sélectionné pour les événements se répétant annuellement.',
+    'YBYSETPOS.required' => 'Vous devez spécifier un jour de la semaine (premier, deuxième, etc...) dans le mois pour les événements se répétant annuellement.',
+
+    'INTERVALS.required' => 'Vous devez saisir des intervalles journaliers pour la répétition par série. Il s\'agit d\'un minimum de deux entiers séparés par des virgules.',
+    'INTERVALS.min' => 'Vous devez saisir des intervalles journaliers pour la répétition par série. Il s\'agit d\'un minimum de deux entiers séparés par des virgules.',
+
+    'name.required' => 'Le champ titre est obligatoire.',
+    'name.min' => 'Le titre doit être au minimum de :min caractères min.',
+
+    'text.required' => 'Le détail de l\'événement est requis.',
+
+    'length.required' => 'Les événements doivent avoir une longueur en heures et minutes dans le format hh:mm (une minute minimum).',
+
+];
